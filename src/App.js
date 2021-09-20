69bb7b9f3d6145aad90d039383d973d19ab453c6
arkaan27 <arkaankanunga@gmail.com>
2021-09-20 15:54:56 +0100

    Added logo

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@
     class App extends Component {
 
      render() {
-            return (
-              <div>
-                   <Contacts />
-              </div>
-            );
-          }
-        }
-
+    return (
+    <div className="App">
+      <header className="App-header">
+        <img src={download} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        </header>
+        </div>
+     );
         export default App;
